61973e62ddc3cfee4f7558dac61e4dbdc6a052b7
Bailly <jeremy.bailly@hotmail.com>
2019-02-22 10:13:30 +0100

    [Add] init project

---
+++ ToDoListv1/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ToDoListv1
+//
+//  Created by lpiem on 22/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ ToDoListv1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ToDoListv1
+//
+//  Created by lpiem on 22/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
